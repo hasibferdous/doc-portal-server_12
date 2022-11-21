92be5f8ca4dbc61ade1a5c0819cd78fd189f4142
hasibferdous <hasibferdous.ict@gmail.com>
2022-11-21 23:22:36 +0600

    booking info by email

--- index.js
+++ index.js
@@ -91,6 +91,13 @@ async function run(){
          * app.delete('/bookings/:id')
         */
 
+                 app.get('/bookings', async (req, res) => {
+                    const email = req.query.email;
+                    const query = { email: email };
+                    const bookings = await bookingsCollection.find(query).toArray();
+                    res.send(bookings);
+                })        
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking);
